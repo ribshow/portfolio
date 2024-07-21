147e636bac6f74ad51af17a7d51a346299b1fe6f
Heryson <belkiorheryson@gmail.com>
2024-07-21 18:18:18 -0300

    update icon

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Rola até a seção portfólios
 document.getElementById("btn-portfolio").addEventListener('click', function(){
     document.getElementById('portfolios').scrollIntoView({behavior: 'smooth'});
 });
