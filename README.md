6db52b5a05fcf5c8e534539d7d98ec527da859d0
Heryson Andrade <141951314+ribshow@users.noreply.github.com>
2024-10-17 14:54:42 -0300

    add

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # portfolio
+
 Meu portfolio
+
+- permitindo o uso de scripts
+- Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 // Rola até a seção portfólios
-document.getElementById("btn-portfolio").addEventListener('click', function(){
-    document.getElementById('portfolios').scrollIntoView({behavior: 'smooth'});
+document.getElementById("btn-portfolio").addEventListener("click", function () {
+  document
+    .getElementById("portfolios")
+    .scrollIntoView({ behavior: "smooth", block: "start" });
+});
+
+// img linkedin
+document.getElementById("linkedin").addEventListener("click", () => {
+  window.open("https://www.linkedin.com/in/heryson-andrade-1b0918284/");
+});
+
+// img github
+document.getElementById("github").addEventListener("click", () => {
+  window.open("https://www.github.com/ribshow");
 });
